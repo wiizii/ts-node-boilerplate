7ee03708329157f1b999f517c5cc0be86c9ebd57
1000ship <cjstjdgur1234@gmail.com>
2022-01-17 14:51:07 +0900

    Customize to me

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 import * as express from 'express';
 
-function runServer() {
-	const app = express();
-	app.listen(3000, () => {
-		console.log('start server');
-	});
-}
-runServer();
+const app = express();
+
+app.listen(3000, () => {
+  console.log('start server');
+});
